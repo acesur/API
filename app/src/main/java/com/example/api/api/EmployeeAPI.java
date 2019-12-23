aba82f533b7e7bd5d7b7366a7e8d7548d710767f
d3rtb0y <sures.chaudhary0@gmail.com>
2019-12-23 11:54:31 +0545

    getter setter for registering employee

--- app/src/main/java/com/example/api/api/EmployeeAPI.java
+++ app/src/main/java/com/example/api/api/EmployeeAPI.java
@@ -1,11 +1,14 @@
 package com.example.api.api;
 
 import com.example.api.model.Employee;
+import com.example.api.model.EmployeeCUD;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface EmployeeAPI {
@@ -16,4 +19,7 @@ public interface EmployeeAPI {
     //Get employee on basis of id
     @GET("employee/{empID}")
     Call<Employee> getEmployeeByID(@Path("empID") int empID);
+
+    @POST("create")
+    Call<Void> registerEmployee(@Body EmployeeCUD emp);
 }
--- app/src/main/java/com/example/api/model/EmployeeCUD.java
+++ app/src/main/java/com/example/api/model/EmployeeCUD.java
@@ -7,10 +7,92 @@ public class EmployeeCUD {
     private int age;
     private String profile_image;
 
-    public EmployeeCUD(String employee_name,float employee_salary, int employee_age){
+    public EmployeeCUD(int id, String employee_name, float employee_salary, int employee_age){
+        this.id = id;
         this.name=employee_name;
         this.salary=employee_salary;
         this.age=employee_age;
 
+    }
+
+    public EmployeeCUD(String name, Float salary, int age) {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public class Employee{
+        private int id;
+        private String employee_name;
+        private float salary;
+        private int age;
+        private String profile_image;
+
+        public Employee(String employee_name, float employee_salary, int employee_age){
+            this.employee_name = employee_name;
+            this.salary=employee_salary;
+            this.age=employee_age;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getEmployee_name() {
+            return employee_name;
+        }
+
+        public void setEmployee_name(String employee_name) {
+            this.employee_name = employee_name;
+        }
+
+        public float getSalary() {
+            return salary;
+        }
+
+        public void setSalary(float salary) {
+            this.salary = salary;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
     }
 }
